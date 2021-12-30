2cefdf07a2f1b72f1a9995f1aa18bb34e02ed40b
Dhaval Kaneriya <dhaval.7@outlook.com>
2021-12-30 23:00:39 +0530

    added buy me a coffee button

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -20,6 +20,18 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Yrsa:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap"
             rel="stylesheet"
           />
+          <script
+            data-name="BMC-Widget"
+            data-cfasync="false"
+            src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+            data-id="kloud.codes"
+            data-description="Support me on Buy me a coffee!"
+            data-message=""
+            data-color="#FF813F"
+            data-position="Right"
+            data-x_margin="18"
+            data-y_margin="18"
+          ></script>
         </Head>
         <body>
           <Main />
